0d232fc66c12a7af83fad2bc0937f20db44fb0a5
NTheDrone-come <unorazmer@gmail.com>
2026-01-04 20:57:04 +0000

    pipis

--- bot.py
+++ bot.py
@@ -399,4 +399,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        print("Бот остановлен.")s
+        print("Бот остановлен.")
